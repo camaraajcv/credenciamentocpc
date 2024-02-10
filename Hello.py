14cfa79057fcef1aede2f843a956ff59a78f6ab7
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-02-10 14:36:46 +0000

    KGJGH

--- Hello.py
+++ Hello.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
+import os
 from datetime import datetime
-from input_mask import IPInputMask, MoneyInputMask
 
 # Função para criar o DataFrame inicial
 def create_dataframe():
@@ -20,27 +20,27 @@ def create_dataframe():
         'CÓDIGO': ['Q2N', 'Q2N', 'Q2W', 'Q2W'],
         'STATUS CREDENCIAMENTO': ['1-Na CPC com pendência da empresa (Ten Aretha). PC ENCAMINHADA PARA A CPC – 23/01/17 2-CREDENCIAMENTO FINALIZADO. PAG ENCONTRA-SE NO ARQUIVO DA PP1.', '1- Encaminhado para CPC em 19/10/2021 (SIGAD 440415), 2- Doc complementar SIGAD 450171.', '1- Encaminhado para CPC,via Chefia imediata, em 13/11/2018; 2 - Retornou para PP1 em 23/04/2019, 3- Solicitação de Inclusão de Elemento de Ligação (documentação INCOMPLETA) -SIGAD 451423.', '1- Encaminhado para CPC em 19/10/2021 (SIGAD 440309)']
     }
-    df = pd.DataFrame(data)
-    return df
+    df2 = pd.DataFrame(data)
+    return df2
 
 # Função para carregar ou criar o DataFrame
 def load_or_create_dataframe():
     if os.path.exists("data.csv"):
         return pd.read_csv("data.csv")
     else:
-        df = create_dataframe()
-        df.to_csv("data.csv", index=False)
-        return df
+        df2 = create_dataframe()
+        df2.to_csv("data.csv", index=False)
+        return df2
 
 def main():
-    st.title('CRUD - Manipulação de DataFrame')
+    st.title('CPC - Controle processos')
 
     # Carregar ou criar o DataFrame
-    df = load_or_create_dataframe()
+    df2 = load_or_create_dataframe()
 
     # Exibir DataFrame atualizado
-    st.header('DataFrame Atualizado:')
-    st.write(df)
+    st.header('Tabela Completa:')
+    st.write(df2)
 
     # Selecionar a ação (Incluir, Editar, Excluir)
     action = st.selectbox('Selecione a ação:', ['Selecione...', 'Incluir', 'Editar', 'Excluir'])
@@ -54,8 +54,9 @@ def main():
             natureza_desconto = st.selectbox('NATUREZA DE DESCONTO:', options=['', 'MENSALIDADE ASSOCIATIVA', 'PREVIDÊNCIA COMPLEMENTAR', 'ASSISTÊNCIA FINANCEIRA', 'CARTÃO DE CRÉDITO', 'Seguro de Vida'], index=0)
             consignataria = st.text_input('CONSIGNATÁRIA:')
             cnpj = st.text_input('CNPJ:')
-            cnpj = cnpj.replace('.', '').replace('-', '').replace('/', '')[:14]  # Remover caracteres indesejados e manter apenas os primeiros 14 dígitos
-            cnpj = f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}' if len(cnpj) >= 12 else cnpj  # Adicionar máscara se CNPJ tiver 14 dígitos
+            cnpj = ''.join(filter(str.isdigit, cnpj))  # Remover caracteres não numéricos
+            if len(cnpj) == 14:
+                cnpj = f'{cnpj[:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:]}'  # Adicionar máscara se CNPJ tiver 14 dígitos
             contrato = st.text_input('NRO CONTRATO (PORTARIA OU TERMO):')
             bca_dou = st.text_input('BCA OU DOU:')
             situacao_atual = st.text_input('SITUAÇÃO:')
@@ -89,38 +90,37 @@ def main():
                         'CÓDIGO': [codigo],
                         'STATUS CREDENCIAMENTO': [status_credenciamento]
                     })
-                    df = pd.concat([df, new_row], ignore_index=True)
-                    df.to_csv("data.csv", index=False)
-                    st.success('Nova linha adicionada com sucesso!')
+                    df2 = pd.concat([df2, new_row], ignore_index=True)
+                    df2.to_csv("data.csv", index=False)
+                    st.success('Novo processo adicionado com sucesso!')
 
         elif action == 'Editar':
-            st.header('Editar Linha Existente:')
-            row_index = st.selectbox('Selecione o número da linha para edição:', options=list(range(len(df))))
-            row = df.iloc[row_index]
-            situacao_edit = st.selectbox('SITUAÇÃO ECONSIG:', options=['', 'Arquivado pela PP1', 'Arquivado pela CPC', 'Análise CPC', 'Encaminhado para CPC', 'Encaminhado para PP1', 'Aguardando Documentações', 'Enviado para Homologação', 'Enviado para Publicação', 'Aguardando assinaturas CPC'], index=['Arquivado pela PP1', 'Arquivado pela CPC', 'Análise CPC', 'Encaminhado para CPC', 'Encaminhado para PP1', 'Aguardando Documentações', 'Enviado para Homologação', 'Enviado para Publicação', 'Aguardando assinaturas CPC'].index(row['SITUAÇÃO ECONSIG']))
-            subprocesso_edit = st.text_input('SUBPROCESSO:', value=row['SUBPROCESSO'])
-            categoria_edit = st.selectbox('CATEGORIA:', options=['', 'I', 'II', 'III'], index=['I', 'II', 'III'].index(row['CATEGORIA']))
-            natureza_desconto_edit = st.selectbox('NATUREZA DE DESCONTO:', options=['', 'MENSALIDADE ASSOCIATIVA', 'PREVIDÊNCIA COMPLEMENTAR', 'ASSISTÊNCIA FINANCEIRA', 'CARTÃO DE CRÉDITO', 'Seguro de Vida'], index=['MENSALIDADE ASSOCIATIVA', 'PREVIDÊNCIA COMPLEMENTAR', 'ASSISTÊNCIA FINANCEIRA', 'CARTÃO DE CRÉDITO', 'Seguro de Vida'].index(row['NATUREZA DE DESCONTO']))
-            consignataria_edit = st.text_input('CONSIGNATÁRIA:', value=row['CONSIGNATÁRIA'])
-            cnpj_edit = st.text_input('CNPJ:', value=row['CNPJ'])
-            contrato_edit = st.text_input('NRO CONTRATO (PORTARIA OU TERMO):', value=row['NRO CONTRATO (PORTARIA OU TERMO)'])
-            bca_dou_edit = st.text_input('BCA OU DOU:', value=row['BCA OU DOU'])
-            situacao_atual_edit = st.text_input('SITUAÇÃO:', value=row['SITUAÇÃO'])
-            data_expiracao_edit = st.date_input('DATA EXPIRAÇÃO CONTRATUAL:', format='DD/MM/YYYY', value=datetime.strptime(row['DATA EXPIRAÇÃO CONTRATUAL'], '%d/%m/%Y'))
+            st.header('Editar processo Existente:')
+            row_index = st.selectbox('Selecione o número da linha do processo para edição:', options=list(range(len(df2))))
+            situacao_edit = st.selectbox('SITUAÇÃO ECONSIG:', options=['', 'Arquivado pela PP1', 'Arquivado pela CPC', 'Análise CPC', 'Encaminhado para CPC', 'Encaminhado para PP1', 'Aguardando Documentações', 'Enviado para Homologação', 'Enviado para Publicação', 'Aguardando assinaturas CPC'], index=['Arquivado pela PP1', 'Arquivado pela CPC', 'Análise CPC', 'Encaminhado para CPC', 'Encaminhado para PP1', 'Aguardando Documentações', 'Enviado para Homologação', 'Enviado para Publicação', 'Aguardando assinaturas CPC'].index(df2.iloc[row_index]['SITUAÇÃO ECONSIG']))
+            subprocesso_edit = st.text_input('SUBPROCESSO:', value=df2.iloc[row_index]['SUBPROCESSO'])
+            categoria_edit = st.selectbox('CATEGORIA:', options=['', 'I', 'II', 'III'], index=['I', 'II', 'III'].index(df2.iloc[row_index]['CATEGORIA']))
+            natureza_desconto_edit = st.selectbox('NATUREZA DE DESCONTO:', options=['', 'MENSALIDADE ASSOCIATIVA', 'PREVIDÊNCIA COMPLEMENTAR', 'ASSISTÊNCIA FINANCEIRA', 'CARTÃO DE CRÉDITO', 'Seguro de Vida'], index=['MENSALIDADE ASSOCIATIVA', 'PREVIDÊNCIA COMPLEMENTAR', 'ASSISTÊNCIA FINANCEIRA', 'CARTÃO DE CRÉDITO', 'Seguro de Vida'].index(df2.iloc[row_index]['NATUREZA DE DESCONTO']))
+            consignataria_edit = st.text_input('CONSIGNATÁRIA:', value=df2.iloc[row_index]['CONSIGNATÁRIA'])
+            cnpj_edit = st.text_input('CNPJ:', value=df2.iloc[row_index]['CNPJ'])
+            contrato_edit = st.text_input('NRO CONTRATO (PORTARIA OU TERMO):', value=df2.iloc[row_index]['NRO CONTRATO (PORTARIA OU TERMO)'])
+            bca_dou_edit = st.text_input('BCA OU DOU:', value=df2.iloc[row_index]['BCA OU DOU'])
+            situacao_atual_edit = st.text_input('SITUAÇÃO:', value=df2.iloc[row_index]['SITUAÇÃO'])
+            data_expiracao_edit = st.date_input('DATA EXPIRAÇÃO CONTRATUAL:', format='DD/MM/YYYY', value=datetime.strptime(df2.iloc[row_index]['DATA EXPIRAÇÃO CONTRATUAL'], '%d/%m/%Y'))
             if data_expiracao_edit <= datetime.today().date():
                 dias_vigencia_edit = 'Expirado'
             else:
                 dias_vigencia_edit = (data_expiracao_edit - datetime.today().date()).days
-            nup_edit = st.text_input('NUP:', value=row['NUP'])
-            codigo_edit = st.text_input('CÓDIGO:', value=row['CÓDIGO'])
-            status_credenciamento_edit = st.text_input('STATUS CREDENCIAMENTO:', value=row['STATUS CREDENCIAMENTO'])
+            nup_edit = st.text_input('NUP:', value=df2.iloc[row_index]['NUP'])
+            codigo_edit = st.text_input('CÓDIGO:', value=df2.iloc[row_index]['CÓDIGO'])
+            status_credenciamento_edit = st.text_input('STATUS CREDENCIAMENTO:', value=df2.iloc[row_index]['STATUS CREDENCIAMENTO'])
 
             # Botão para editar linha existente
             if st.button('Editar'):
                 if situacao_edit == '' or subprocesso_edit == '' or categoria_edit == '' or natureza_desconto_edit == '' or consignataria_edit == '' or cnpj_edit == '' or contrato_edit == '' or bca_dou_edit == '' or situacao_atual_edit == '' or nup_edit == '' or codigo_edit == '' or status_credenciamento_edit == '':
                     st.warning('Todos os campos devem ser preenchidos!')
                 else:
-                    df.loc[row_index] = {
+                    df2.loc[row_index] = {
                         'SITUAÇÃO ECONSIG': situacao_edit,
                         'SUBPROCESSO': subprocesso_edit,
                         'CATEGORIA': categoria_edit,
@@ -136,17 +136,17 @@ def main():
                         'CÓDIGO': codigo_edit,
                         'STATUS CREDENCIAMENTO': status_credenciamento_edit
                     }
-                    df.to_csv("data.csv", index=False)
+                    df2.to_csv("data.csv", index=False)
                     st.success('Linha editada com sucesso!')
 
         elif action == 'Excluir':
-            st.header('Excluir Linha Existente:')
-            row_index = st.selectbox('Selecione o número da linha para exclusão:', options=list(range(len(df))))
+            st.header('Excluir Processo Existente:')
+            row_index = st.selectbox('Selecione o número da linha para exclusão:', options=list(range(len(df2))))
             st.write(f'Linha selecionada para exclusão: {row_index}')
             if st.button('Excluir'):
-                df.drop(row_index, inplace=True)
-                df.to_csv("data.csv", index=False)
-                st.success('Linha excluída com sucesso!')
+                df2.drop(row_index, inplace=True)
+                df2.to_csv("data.csv", index=False)
+                st.success('Processo excluída com sucesso!')
 
 if __name__ == "__main__":
     main()
