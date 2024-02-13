9aaebaac31af9407e2e1d4792e02f8c961e6339c
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-02-13 22:14:12 +0000

    sdfsdf

--- Hello.py
+++ Hello.py
@@ -5,7 +5,21 @@ import re
 from datetime import datetime, date
 import matplotlib.pyplot as plt
 import seaborn as sns
+# URL da imagem
+image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
 
+#Código HTML e CSS para ajustar a largura da imagem para 20% da largura da coluna e centralizar
+html_code = f'<div style="display: flex; justify-content: center;"><img src="{image_url}" alt="Imagem" style="width:8vw;"/></div>'
+# Exibir a imagem usando HTML
+st.markdown(html_code, unsafe_allow_html=True)
+
+# Centralizar o texto abaixo da imagem
+st.markdown("<h1 style='text-align: center; font-size: 1.5em;'>DIRETORIA DE ADMINISTRAÇÃO DA AERONÁUTICA</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; font-size: 1.2em;'>SUBDIRETORIA DE PAGAMENTO DE PESSOAL</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; font-size: 1em; text-decoration: underline;'>PP1 - DIVISÃO DE DESCONTOS</h3>", unsafe_allow_html=True)
+
+# Texto explicativo
+st.write("CPC - Comissão Permanente de Credenciamento")
 # Função para carregar ou criar o DataFrame
 def carregar_dataframe():
     if os.path.exists("dados.csv"):
@@ -200,7 +214,7 @@ def main():
                         st.success('Dados alterados com sucesso.')
 
     # Exibir DataFrame atualizado
-    st.header('DataFrame Atualizado')
+    st.header('Processos Atualizado')
     st.write(df)
 
     # Salvar DataFrame em arquivo CSV
@@ -217,11 +231,20 @@ def main():
     count_by_category = df['CATEGORIA'].value_counts()
     st.bar_chart(count_by_category)
 
+    st.subheader('Percentual por Natureza de Desconto')
+    count_by_natureza = df['NATUREZA DE DESCONTO'].value_counts()
+    st.write(count_by_natureza)
+    plt.figure(figsize=(8, 6))
+    plt.pie(count_by_natureza, labels=count_by_natureza.index, autopct='%1.1f%%', startangle=140)
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    st.pyplot()
+
     st.header('Gráficos')
     st.subheader('Correlação entre Dias para Fim Vigência e CPC Anual')
     fig, ax = plt.subplots()
     sns.scatterplot(data=df, x='Dias para Fim Vigência', y='CPC ANUAL', ax=ax)
     st.pyplot(fig)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
