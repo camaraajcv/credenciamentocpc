91a34a8c5f5749652b0605989dfac1f71b5c1d49
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-02-13 20:06:14 +0000

    zxczxc

--- Hello.py
+++ Hello.py
@@ -44,15 +44,15 @@ def main():
 
         with col1:
             situacao_econsig = st.selectbox('Situação Econômica*', options=['', 'Recredenciado', 'Credenciado', 'Aguardando Publicação', 'Arquivado'])
-            localizacao = st.text_input('Localização', required=False)
-            consignataria = st.text_input('Consignatária*', required=True)
-            bca_ou_dou = st.text_input('BCA ou DOU', required=False)
-            situacao = st.text_input('Situação*', required=True)
+            localizacao = st.text_input('Localização')
+            consignataria = st.text_input('Consignatária*')
+            bca_ou_dou = st.text_input('BCA ou DOU')
+            situacao = st.text_input('Situação*')
             data_expiracao_contratual = st.date_input('Data Expiração Contratual*', format='DD/MM/YYYY')
             categoria = st.selectbox('Categoria*', options=['', 'I', 'II', 'III'])
             natureza_desconto = st.selectbox('Natureza de Desconto*', options=['', 'MENSALIDADE ASSOCIATIVA', 'PREVIDÊNCIA COMPLEMENTAR', 'ASSISTÊNCIA FINANCEIRA','CARTÃO DE CRÉDITO', 'SEGURO DE VIDA'])
             cnpj = st.text_input('CNPJ*', placeholder='XX.XXX.XXX/XXXX-XX')
-            nro_contrato = st.text_input('Nro Contrato (Portaria ou Termo)', required=False)
+            nro_contrato = st.text_input('Nro Contrato (Portaria ou Termo)')
             verificado = st.selectbox('Verificado?*', options=['', 'Sim', 'Não'])
         with col2:
             data_atual = date.today()  # Obtém a data atual
@@ -64,41 +64,44 @@ def main():
            
             dias_para_fim_vigencia = st.text_input('Dias para Fim Vigência', value=dias_para_fim_vigencia, disabled=True)
             nup = st.text_input('NUP')
-            codigo = st.text_input('Código', required=False)
+            codigo = st.text_input('Código')
             status_credenciamento = st.text_input('Status Credenciamento')
-            acao = st.text_input('Ação', required=False)
-            oficio_para_ec = st.text_input('Ofício para EC', required=False)
+            acao = st.text_input('Ação')
+            oficio_para_ec = st.text_input('Ofício para EC')
             cpc_status = st.selectbox('CPC Status', options=['','EM ANÁLISE', 'CONCLUÍDO', 'ENTREGUE', 'REJEITADO'])
             cpc_anual = st.selectbox('CPC Anual', options=['', 'CPC 2021', 'CPC 2022', 'CPC 2023', 'CPC 2024', 'CPC 2025', 'CPC 2026'])
 
         if st.button('Inserir'):
             if validar_cnpj(cnpj):
-                novo_dado = {
-                    'SITUAÇÃO ECONSIG': situacao_econsig,
-                    'LOCALIZAÇÃO': localizacao,
-                    'CATEGORIA': categoria,
-                    'NATUREZA DE DESCONTO': natureza_desconto,
-                    'CONSIGNATÁRIA': consignataria,
-                    'CNPJ': cnpj,
-                    'NRO CONTRATO (PORTARIA OU TERMO)': nro_contrato,
-                    'BCA OU DOU': bca_ou_dou,
-                    'SITUAÇÃO': situacao,
-                    'DATA EXPIRAÇÃO CONTRATUAL': data_expiracao_contratual.strftime('%d/%m/%Y'),
-                    'Dias para Fim Vigência': dias_para_fim_vigencia,
-                    'NUP': nup,
-                    'CÓDIGO': codigo,
-                    'STATUS CREDENCIAMENTO': status_credenciamento,
-                    'AÇÃO': acao,
-                    'OFÍCIO PARA EC': oficio_para_ec,
-                    'CPC STATUS': cpc_status,
-                    'Verificado ?': verificado,
-                    'CPC ANUAL': cpc_anual
-                }
+                if consignataria.strip() == '' or situacao.strip() == '' or situacao_econsig.strip() == '' or verificado.strip() == '':
+                    st.error('Os campos marcados com * são obrigatórios.')
+                else:
+                    novo_dado = {
+                        'SITUAÇÃO ECONSIG': situacao_econsig,
+                        'LOCALIZAÇÃO': localizacao,
+                        'CATEGORIA': categoria,
+                        'NATUREZA DE DESCONTO': natureza_desconto,
+                        'CONSIGNATÁRIA': consignataria,
+                        'CNPJ': cnpj,
+                        'NRO CONTRATO (PORTARIA OU TERMO)': nro_contrato,
+                        'BCA OU DOU': bca_ou_dou,
+                        'SITUAÇÃO': situacao,
+                        'DATA EXPIRAÇÃO CONTRATUAL': data_expiracao_contratual.strftime('%d/%m/%Y'),
+                        'Dias para Fim Vigência': dias_para_fim_vigencia,
+                        'NUP': nup,
+                        'CÓDIGO': codigo,
+                        'STATUS CREDENCIAMENTO': status_credenciamento,
+                        'AÇÃO': acao,
+                        'OFÍCIO PARA EC': oficio_para_ec,
+                        'CPC STATUS': cpc_status,
+                        'Verificado ?': verificado,
+                        'CPC ANUAL': cpc_anual
+                    }
 
-                novo_df = pd.DataFrame([novo_dado])
-                df = pd.concat([df, novo_df], ignore_index=True)
+                    novo_df = pd.DataFrame([novo_dado])
+                    df = pd.concat([df, novo_df], ignore_index=True)
 
-                st.success('Dados inseridos com sucesso.')
+                    st.success('Dados inseridos com sucesso.')
 
     # Exibir DataFrame atualizado
     st.header('DataFrame Atualizado')
@@ -109,4 +112,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
