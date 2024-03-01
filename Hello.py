20f3cae6537a8083dbc2afda8c44c343ca5181f2
camaraajcv <camarafa@gmail.com>
2024-03-01 12:48:54 -0300

    sdfsdf

--- Hello.py
+++ Hello.py
@@ -289,11 +289,8 @@ def main():
 
     # Exibir DataFrame atualizado
     st.header('Processos Atualizados')
-    # Remover vírgulas da coluna "SUBPROCESSO SILOMS" e converter para inteiro
-    df['SUBPROCESSO SILOMS'] = df['SUBPROCESSO SILOMS'].str.replace(',', '').astype(int)
+    st.write(df)
 
-    # Exibir o DataFrame e usar uma função lambda para remover as vírgulas ao exibir
-    st.write(df.applymap(lambda x: '{:,.0f}'.format(x) if isinstance(x, (int, float)) else x), index=False)
     # Salvar DataFrame em arquivo CSV
     salvar_dataframe(df)
     # Configuração para desativar o aviso PyplotGlobalUseWarning
