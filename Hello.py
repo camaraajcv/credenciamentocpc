7c63f6c6fe4b55cea8e370d0e96e4eb5f754fa00
Alex Camara <camarafa@gmail.com>
2024-03-16 14:06:55 -0300

    sdfsdfsd

--- Hello.py
+++ Hello.py
@@ -11,7 +11,7 @@ import openpyxl
 
 # URL da imagem
 image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
-excel_url="https://raw.githubusercontent.com/camaraajcv/credencaimentocpc/main/dados_cpc.xlsx"
+excel_url="https://raw.githubusercontent.com/camaraajcv/credenciamentocpc/main/dados_cpc.xlsx"
 #Código HTML e CSS para ajustar a largura da imagem para 20% da largura da coluna e centralizar
 html_code = f'<div style="display: flex; justify-content: center;"><img src="{image_url}" alt="Imagem" style="width:8vw;"/></div>'
 # Exibir a imagem usando HTML
