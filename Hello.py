b0c02cb13998aeb67195638d2107db8d26152114
Alex Camara <camarafa@gmail.com>
2024-03-16 18:07:57 -0300

    sdfsdffs

--- Hello.py
+++ Hello.py
@@ -2,17 +2,31 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    st.title('Aplicativo para Visualização de Dados Excel')
+    st.title('Aplicativo para Visualização e Edição de Dados Excel')
 
     # URL do arquivo Excel no GitHub
     excel_url = "dados_cpc.xlsx"
 
-    # Lê o arquivo Excel diretamente da URL
+    # Carrega o DataFrame a partir do Excel
     df = pd.read_excel(excel_url)
 
-    # Mostrar os dados na tabela
+    # Mostra os dados na tabela
     st.write("## Dados do Excel")
     st.write(df)
+
+    # Opção para selecionar linhas para exclusão
+    rows_to_delete = st.multiselect("Selecione as linhas para excluir", df.index)
+
+    if st.button("Excluir Linhas"):
+        # Exclui as linhas selecionadas do DataFrame
+        df = df.drop(index=rows_to_delete)
+        
+        # Atualiza o arquivo Excel com as linhas excluídas (opcional)
+        df.to_excel(excel_url, index=False)  # Descomente esta linha se desejar atualizar o arquivo Excel no GitHub
+        
+        # Mostra os dados atualizados na tabela
+        st.write("## Dados do Excel Atualizados")
+        st.write(df)
 
 if __name__ == "__main__":
     main()
