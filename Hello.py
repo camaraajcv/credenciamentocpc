18d6983c704ccb9fdc73e724c05e538a90ec163a
Alex Camara <camarafa@gmail.com>
2024-03-24 17:24:59 -0300

    sdfsdfds

--- Hello.py
+++ Hello.py
@@ -5,6 +5,21 @@ from datetime import date
 import re
 import warnings
 
+# URL da imagem
+image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
+
+#Código HTML e CSS para ajustar a largura da imagem para 20% da largura da coluna e centralizar
+html_code = f'<div style="display: flex; justify-content: center;"><img src="{image_url}" alt="Imagem" style="width:8vw;"/></div>'
+# Exibir a imagem usando HTML
+st.markdown(html_code, unsafe_allow_html=True)
+
+# Centralizar o texto abaixo da imagem
+st.markdown("<h1 style='text-align: center; font-size: 1.5em;'>DIRETORIA DE ADMINISTRAÇÃO DA AERONÁUTICA</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; font-size: 1.2em;'>SUBDIRETORIA DE PAGAMENTO DE PESSOAL</h2>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; font-size: 1em; text-decoration: underline;'>PP1 - DIVISÃO DE DESCONTOS</h3>", unsafe_allow_html=True)
+
+# Texto explicativo
+st.write("CPC - Comissão Permanente de Credenciamento")
 # Suprimindo o aviso específico
 warnings.filterwarnings("ignore", message="pandas only supports SQLAlchemy connectable")
 # Função para inserir dados no banco de dados
