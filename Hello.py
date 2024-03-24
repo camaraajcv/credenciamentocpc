d83dc6793198de320c3a9d36f1d9836e25337ce2
Alex Camara <camarafa@gmail.com>
2024-03-24 18:43:52 -0300

    sfsdfsfd

--- Hello.py
+++ Hello.py
@@ -346,12 +346,19 @@ def main():
                 
                 # Corrigindo a entrada de data para data_expiracao_contratual_edit e data_entrada_edit
                 data_expiracao_contratual_edit = st.date_input('Data de Expiração Contratual', value=pd.to_datetime(data_to_edit[10], errors='coerce'))
+                if isinstance(data_expiracao_contratual_edit, pd.Timestamp):
+                    data_expiracao_contratual_edit = data_expiracao_contratual_edit.date()
+                
                 codigo_edit = st.text_input('Código', value=data_to_edit[11])
                 status_credenciamento_edit = st.text_input('Status de Credenciamento', value=data_to_edit[12])
                 cpc_status_edit = st.text_input('CPC Status', value=data_to_edit[13])
                 cpc_anual_edit = st.text_input('CPC Anual', value=data_to_edit[14])
+                
                 # Corrigindo a entrada de data para data_entrada_edit
                 data_entrada_edit = st.date_input('Data de Entrada', value=pd.to_datetime(data_to_edit[15], errors='coerce'))
+                if isinstance(data_entrada_edit, pd.Timestamp):
+                    data_entrada_edit = data_entrada_edit.date()
+
 
                 if st.button('Atualizar Registro'):
                     # Atualiza o registro no banco de dados
